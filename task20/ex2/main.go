180968ea497997c734e7a77808bb7f8b50e0ad13
muratkadirbay-droid <muratkadirbay@gmail.com>
2026-06-23 12:36:50 +0500

    Urok 20

--- task20/ex2/main.go
+++ task20/ex2/main.go
@@ -15,8 +15,13 @@ func (s Student) Average() float64 {
 	return float64(sum) / float64(len(s.Grades))
 }
 
+func (s Student) GetName() string {
+	return s.Name
+}
+
 type Person interface {
 	Average() float64
+	GetName() string
 }
 
 func main() {
@@ -24,9 +29,9 @@ func main() {
 	s2 := Student{"Dias", []int{70, 75, 80}}
 	s3 := Student{"Alina", []int{95, 90, 100}}
 
-	people := []Student{s1, s2, s3}
+	people := []Person{s1, s2, s3}
 
 	for _, p := range people {
-		fmt.Printf("Студент %s: средний балл %.2f\n", p.Name, p.Average())
+		fmt.Printf("Студент %s: средний балл %.2f\n", p.GetName(), p.Average())
 	}
 }
